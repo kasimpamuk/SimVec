c4c719c3823bf4b8344716262bb703e541bbb8c4
alpertools <alper_tools@outlook.com>
2023-11-25 00:33:47 +0300

    build: create base java maven project

---
+++ src/main/java/io/gitlab/group23/simvec/SimvecApplication.java
@@ -0,0 +1,13 @@
+package io.gitlab.group23.simvec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimvecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimvecApplication.class, args);
+	}
+
+}
