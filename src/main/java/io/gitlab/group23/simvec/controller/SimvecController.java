9fe31d951589d101cfef9e55d10784d7c6b9df09
e2448595 <e2448595@ceng.metu.edu.tr>
2023-11-28 23:33:45 +0300

    SPRINT-2: create necessary endpoints to be implemented in future

--- src/main/java/io/gitlab/group23/simvec/controller/SimvecController.java
+++ src/main/java/io/gitlab/group23/simvec/controller/SimvecController.java
@@ -1,5 +1,35 @@
 package io.gitlab.group23.simvec.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/")
 public class SimvecController {
-	// this is the controller...
+
+	@GetMapping("server-control")
+	public String serverControl() {
+		return "The server is up.";
+	}
+
+	@GetMapping("text-based-search")
+	public String textBasedSearch() {
+		// TODO: Text based search logic
+		return "Hit text based search endpoint";
+	}
+
+	@GetMapping("image-based-search")
+	public String imageBasedSearch() {
+		// TODO: Image based search logic
+		return "Hit image based search endpoint";
+	}
+
+	@GetMapping("cloud-synchronization")
+	public String cloudSynchronization() {
+		// This can be implemented as a cron job
+		// TODO: Cloud synchronization logic
+		return "Hit cloud syncronization endpoint";
+	}
+
 }
