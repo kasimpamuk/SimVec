cc456e684fbc8f3536f167f79208bdbd3e6c32e0
ATAKAN GELECEK <e2521599@ceng.metu.edu.tr>
2023-12-15 08:53:36 +0000

    Update views.py

--- vector-database-api/api_for_database/DatabaseApi/views.py
+++ vector-database-api/api_for_database/DatabaseApi/views.py
@@ -1,4 +1,4 @@
-# Django view for handling image or text conversion with external services
+# Django view for handling image or text conversion
 import requests
 import csv
 from glob import glob
@@ -9,7 +9,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 from towhee import pipe, ops, DataCollection
 from pymilvus import connections, FieldSchema, CollectionSchema, DataType, Collection, utility
-from django.core.files.storage import FileSystemStorage
+#from django.core.files.storage import FileSystemStorage
 
 # Towhee parameters
 MODEL = 'resnet50'
@@ -25,8 +25,8 @@ INDEX_TYPE = 'IVF_FLAT'
 METRIC_TYPE = 'L2'
 
 # path to csv (column_1 indicates image path) OR a pattern of image paths
-INSERT_SRC = '../data/reverse_image_search.csv'
-QUERY_SRC = '../data/test/*/*.JPEG'
+INSERT_SRC = 'reverse_image_search.csv'
+QUERY_SRC = 'test/*/*.JPEG'
 
 # Load image path
 def load_image(x):
@@ -57,53 +57,82 @@ p_insert = (
           .output('mr')
     )
 
+# Search pipeline
+p_search_pre = (
+        p_embed.map('vec', ('search_res'), ops.ann_search.milvus_client(
+                    host=HOST, port=PORT, limit=TOPK,
+                    collection_name=COLLECTION_NAME))
+               .map('search_res', 'pred', lambda x: [str(Path(y[0]).resolve()) for y in x])
+#                .output('img_path', 'pred')
+)
+p_search = p_search_pre.output('img_path', 'pred')
 
-p_insert(INSERT_SRC)
+def serialize_data_queue(data_queue):
+    # Convert DataQueue object to a serializable format (e.g., a list or a dict)
+    return {'data': data_queue}
 
-
-def get_embeddings(data, data_type):
+def search_image(data, data_type, collection):
     if data_type == 'image':
-        pass
+        # Search for example query image(s)
+        collection.load()
+        dc = p_search(data)
+        return dc
         
-    elif data_type == 'text':  # text
+    elif data_type == 'text':  
         pass
-        # Placeholder for text processing - replace with actual model logic
-        #embeddings = your_text_model_function(data)
+        # Placeholder for text processing 
+        
 
-    return [1, 2, 3, 4, 5] 
 
-def store_in_vector_db(embeddings):
-    """
-    Store embeddings in the internal vector database.
+# Create milvus collection (delete first if exists)
+def create_milvus_collection(collection_name, dim):
+    if utility.has_collection(collection_name):
+        utility.drop_collection(collection_name)
+    
+    fields = [
+        FieldSchema(name='path', dtype=DataType.VARCHAR, description='path to image', max_length=500, 
+                    is_primary=True, auto_id=False),
+        FieldSchema(name='embedding', dtype=DataType.FLOAT_VECTOR, description='image embedding vectors', dim=dim)
+    ]
+    schema = CollectionSchema(fields=fields, description='reverse image search')
+    collection = Collection(name=collection_name, schema=schema)
 
-    :param embeddings: Embeddings to be stored.
-    :return: Identifier of the stored embeddings.
-    """
-    # Placeholder for database storage logic - replace with actual DB logic
-    #stored_id = your_database_storage_function(embeddings)
+    index_params = {
+        'metric_type': METRIC_TYPE,
+        'index_type': INDEX_TYPE,
+        'params': {"nlist": 2048}
+    }
+    collection.create_index(field_name='embedding', index_params=index_params)
+    return collection
 
-    #return stored_id
-    return 1
+def initialize_milvus():
+    connections.connect(host=HOST, port=PORT)
 
+    # Create collection
+    collection = create_milvus_collection(COLLECTION_NAME, DIM)
+    print(f'A new collection created: {COLLECTION_NAME}')
+
+    # Insert data
+    p_insert(INSERT_SRC)
+    print('Number of data inserted:', collection.num_entities)
+    return collection
 
 @csrf_exempt
 @require_http_methods(["POST"])
 def image_conversion(request):
-    # BURASI DÜZELTİLEBİLİR
-    print(request.POST)
-    try:
-        # Determine if the request has an image or text
-        if 'image' in request.FILES:
-            image = request.FILES['image']
-            
-            # Process image using ML model
-            embeddings = get_embeddings(image, 'image')
+    # Connect to Milvus service
+    collection = initialize_milvus()
 
-            # Store embeddings in external vector database
-            stored_id = store_in_vector_db(embeddings)
-            return JsonResponse({'message': 'Image processed successfully', 'stored_id': stored_id})
-        else:
-            return JsonResponse({'error': 'No valid image provided'}, status=400)
+    try:
+        image = request.body.decode('utf-8')
+        QUERY_SRC = image
+        
+        # Process image using ML model
+        result_path_list = search_image(QUERY_SRC, 'image', collection).to_list()
+        
+        # return result_path_list as response
+        return JsonResponse({'message': 'Image processed successfully', 'stored_id': result_path_list})
+    
     except Exception as e:
         # Handle any errors that occur during the process
         return JsonResponse({'error': str(e)}, status=500)
@@ -114,6 +143,7 @@ def image_conversion(request):
 def text_conversion(request):
     #print(request.body.decode('utf-8'))
     return JsonResponse({'message': 'Text conversion hit'})
+    """
     try:
         # Determine if the request has an image or text
         text = request.body.decode('utf-8')
@@ -131,4 +161,4 @@ def text_conversion(request):
 
     except Exception as e:
         # Handle any errors that occur during the process
-        return JsonResponse({'error': str(e)}, status=500)
+        return JsonResponse({'error': str(e)}, status=500) """
