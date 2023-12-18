c910a6368c79c26f3806da6c0f789899a431be1f
e2448694 <e2448694@ceng.metu.edu.tr>
2023-12-19 01:53:37 +0300

    SPRINT 3: Backend Logic

--- Backend/src/main/java/io/gitlab/group23/simvec/exceptions/GlobalExceptionHandler.java
+++ Backend/src/main/java/io/gitlab/group23/simvec/exceptions/GlobalExceptionHandler.java
@@ -4,20 +4,36 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.validation.FieldError;
 import org.springframework.http.HttpStatus;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        // todo: logic
-        return ResponseEntity.badRequest().body("Validation error");
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.badRequest().body(errors);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleOtherExceptions(Exception ex) {
-        // logic will be added
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred");
+        // Log the debug
+        // log.error("Unexpected error occurred", ex);
+
+        // Generic error message sent to the client
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("An unexpected error occurred. Please try again later.");
     }
+
+    // more to be added
+
 }
