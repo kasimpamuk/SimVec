b03fa496eb33186a64926879f9469d238e990e5f
ATAKAN GELECEK <e2521599@ceng.metu.edu.tr>
2023-12-18 21:13:30 +0000

    SPRINT 3: Added image embedding and storage endpoint URL to urls.py file

--- vector-database-api/api_for_database/DatabaseApi/urls.py
+++ vector-database-api/api_for_database/DatabaseApi/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import image_conversion, text_conversion
+from .views import image_based_search, text_based_search, image_embedding_and_storage
 
 urlpatterns = [
     path('api/image_based_search', image_based_search, name='image_based_search'),
     path('api/text_based_search', text_based_search, name='text_based_search'),
+    path('api/image_embedding_and_storage', image_embedding_and_storage, name='image_embedding_and_storage'),
 ]
