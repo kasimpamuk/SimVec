a28f546b2e65c089a11914b77912bfbd871f64dd
ELIF LALE <e2448694@ceng.metu.edu.tr>
2023-12-26 18:09:13 +0000

    Update App.js

--- Simvec-frontend/simvec-frontend/src/App.js
+++ Simvec-frontend/simvec-frontend/src/App.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import './App.css'; // Make sure this path is correct
-import logo from './simvec.png'; // Adjust the path based on your folder structure
+import './App.css';
+import logo from './simvec.png';
 
 function ImageUpload() {
   const [image, setImage] = useState(null);
   const [preview, setPreview] = useState(null);
-  const [displayedImage, setDisplayedImage] = useState(null);
-
+  const [imageList, setImageList] = useState([]);
+  const [text, setText] = useState('');
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -24,38 +24,87 @@ function ImageUpload() {
     formData.append('file', image);
 
     try {
-      const response = await fetch('http://localhost:8080/api/upload', {
+      const response = await fetch('http://localhost:8080/api/image-based-search', {
         method: 'POST',
         body: formData,
       });
-      const fileData = await response.blob();
-      const imageObjectURL = URL.createObjectURL(fileData);
-      setDisplayedImage(imageObjectURL);
+      const imageBlobs = await response.json(); // Assuming the response is an array of blobs
+      const urls = imageBlobs.map(blob => URL.createObjectURL(blob));
+      setImageList(urls);
     } catch (error) {
       alert("Error uploading image");
     }
   };
 
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleTextSubmit = async (e) => {
+    e.preventDefault();
+    if (!text) {
+      alert("Please enter some text");
+      return;
+    }
+
+    try {
+      const response = await fetch('http://localhost:8080/api/text-based-search', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ text }),
+      });
+      const imageBlobs = await response.json(); // Assuming the response is an array of blobs
+      const urls = imageBlobs.map(blob => URL.createObjectURL(blob));
+      setImageList(urls);
+    } catch (error) {
+      alert("Error processing text");
+    }
+  };
+
   return (
-    <div className="image-upload-container">
+    <div className="container">
       <img src={logo} alt="Logo" className="website-logo" />
-      <form onSubmit={handleSubmit}>
-        <input 
-          type="file" 
-          onChange={handleImageChange} 
-          style={{ display: 'none' }} 
-          id="file-upload"
-        />
-        <label htmlFor="file-upload" className="image-upload-label">
-          {preview ? <img src={preview} alt="Preview" className="image-preview" /> : "Click to select an image"}
-        </label>
-        <br />
-        <button type="submit" className="upload-btn">Upload</button>
-      </form>
-      {displayedImage && (
-        <div>
-          <p>Uploaded Image:</p>
-          <img src={displayedImage} alt="Uploaded" className="uploaded-image" />
+  
+      {/* Image Upload Section */}
+      <div className="image-upload-container">
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="file" 
+            onChange={handleImageChange} 
+            style={{ display: 'none' }} 
+            id="file-upload"
+          />
+          <label htmlFor="file-upload" className="image-upload-label">
+            {preview ? <img src={preview} alt="Preview" className="image-preview" /> : "Click to select an image"}
+          </label>
+          <br />
+          <button type="submit" className="upload-btn">Upload</button>
+        </form>
+      </div>
+  
+      {/* Text Submission Section */}
+      <div className="text-submission-container">
+        <form onSubmit={handleTextSubmit}>
+          <textarea
+            value={text}
+            onChange={handleTextChange}
+            placeholder="Enter text here"
+            className="text-input"
+          />
+          <br />
+          <button type="submit" className="submit-btn">Submit Text</button>
+        </form>
+      </div>
+  
+      {/* Displaying Returned Images */}
+      {imageList.length > 0 && (
+        <div className="image-list-container">
+          <h3>Returned Images:</h3>
+          {imageList.map((imgSrc, index) => (
+            <img key={index} src={imgSrc} alt={`Result ${index}`} className="returned-image" />
+          ))}
         </div>
       )}
     </div>
