e68a2b082bbc9868d32283860dbdc3e74ff0da17
e2448595 <e2448595@ceng.metu.edu.tr>
2024-01-09 23:30:57 +0300

    SPRINT-4: complete the implementation of authentication

--- Backend/src/main/java/io/gitlab/group23/simvec/config/authentication/JwtAuthorizationFilter.java
+++ Backend/src/main/java/io/gitlab/group23/simvec/config/authentication/JwtAuthorizationFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -27,6 +28,7 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
 	private final JwtUtil jwtUtil;
 	private final ObjectMapper mapper;
 
+	@Autowired
 	public JwtAuthorizationFilter(JwtUtil jwtUtil, ObjectMapper mapper) {
 		this.jwtUtil = jwtUtil;
 		this.mapper = mapper;
