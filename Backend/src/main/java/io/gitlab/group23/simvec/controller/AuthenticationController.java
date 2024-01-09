e277576598bb8dbc7bd94eb07d49f4858edd0b00
e2448595 <e2448595@ceng.metu.edu.tr>
2024-01-09 22:14:48 +0300

    SPRINT-4: create a controller dedicated to authentication

---
+++ Backend/src/main/java/io/gitlab/group23/simvec/controller/AuthenticationController.java
@@ -0,0 +1,14 @@
+package io.gitlab.group23.simvec.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/authentication")
+@CrossOrigin
+public class AuthenticationController {
+
+
+
+}
