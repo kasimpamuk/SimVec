13b3879e20146ef1fae768e0245a8a2fb8fc36d4
e2448694 <e2448694@ceng.metu.edu.tr>
2024-02-27 18:39:27 +0300

    register page and page direction

--- Simvec-frontend/simvec-frontend/src/LoginPage.js
+++ Simvec-frontend/simvec-frontend/src/LoginPage.js
@@ -14,45 +14,49 @@ function LoginPage() {
     console.log('Logging in with:', username, password);
 
     // If login is successful, navigate to the main page
-    // Replace '/main-page' with the actual path you want to navigate to after login
     navigate('/main-page');
+  };
+
+  const handleRegisterClick = (event) => {
+    event.preventDefault(); // Prevent form submission
+    navigate('/register-page'); // Navigate to register page
   };
 
   return (
     <>
-    <div className='header'>
-    <img src={logo} alt="Logo" className="website-logo" />
-    </div>
-    
-    <div className="login-container">
-    
-      <form onSubmit={handleSubmit}>
-        <h2 className="login-text">Login</h2>
-        <div className="form-group">
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    </div>
+      <div className='header'>
+        <img src={logo} alt="Logo" className="website-logo" />
+      </div>
+      
+      <div className="login-container">
+        <form onSubmit={handleSubmit}>
+          <h2 className="login-text">Login</h2>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit">Login</button>
+          <button onClick={handleRegisterClick}>Register</button>
+        </form>
+      </div>
     </>
   );
 }
