f494178e098745391ff4fc8df406645848c9e01f
ATAKAN GELECEK <e2521599@ceng.metu.edu.tr>
2024-03-11 20:37:40 +0000

    SPRINT-6: Implemented SecurityConfig class for OAuth2

--- Backend/src/main/java/io/gitlab/group23/simvec/config/SecurityConfig.java
+++ Backend/src/main/java/io/gitlab/group23/simvec/config/SecurityConfig.java
@@ -1,22 +1,27 @@
 package io.gitlab.group23.simvec.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
+@Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests(authorizeRequests ->
-                        authorizeRequests
-                                .antMatchers("/public/**").permitAll()
-                                .anyRequest().authenticated()  // Other URLs are protected
-                )
-                .oauth2Login();  // Enable OAuth2 login
-    }
-}
-
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .defaultSuccessUrl("http://localhost:3000/main-page", true)
+                );
+        return http.build();
+    }
 }
