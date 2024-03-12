d549e6d72e55dc1af7b7097495dbb5811b09d8b3
ATAKAN GELECEK <e2521599@ceng.metu.edu.tr>
2024-03-12 17:00:04 +0000

    Update urls.py

--- VectorDatabase/api_for_database/DatabaseApi/urls.py
+++ VectorDatabase/api_for_database/DatabaseApi/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import image_based_search, text_based_search, image_embedding_and_storage
+from .views import image_based_search, text_based_search, image_embedding_and_storage, create_collection_for_new_user
 
 urlpatterns = [
     path('api/image-based-search', image_based_search, name='image_based_search'),
     path('api/text-based-search', text_based_search, name='text_based_search'),
     path('api/image-embedding-and-storage', image_embedding_and_storage, name='image_embedding_and_storage'),
+    path('api/create-collection', create_collection_for_new_user, name='create_collection_for_new_user')
 ]
 
