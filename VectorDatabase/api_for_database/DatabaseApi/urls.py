a81e45157b1f4d71b2bc46d217d0c8ea19359add
ATAKAN GELECEK <e2521599@ceng.metu.edu.tr>
2024-03-28 20:48:42 +0000

    SPRINT-7: Minor update on endpoint naming

--- VectorDatabase/api_for_database/DatabaseApi/urls.py
+++ VectorDatabase/api_for_database/DatabaseApi/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('api/image-based-search', image_based_search, name='image_based_search'),
     path('api/text-based-search', text_based_search, name='text_based_search'),
     path('api/image-embedding-and-storage', image_embedding_and_storage, name='image_embedding_and_storage'),
-    path('api/create-collection', create_collection_for_new_user, name='create_collection_for_new_user')
+    path('api/create-collection-and-embeddings', create_collection_for_new_user, name='create_collection_for_new_user')
 ]
 
