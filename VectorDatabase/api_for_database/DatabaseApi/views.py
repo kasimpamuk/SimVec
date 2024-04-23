883dd0bd3f6523ce9bd996dd88b85eb987ebc88e
MUHAMMET KASIM PAMUK <e2375657@ceng.metu.edu.tr>
2024-04-23 21:07:44 +0000

    use models from predetermined local caches

--- VectorDatabase/api_for_database/DatabaseApi/views.py
+++ VectorDatabase/api_for_database/DatabaseApi/views.py
@@ -17,8 +17,9 @@ import torch
 from transformers import CLIPProcessor, CLIPModel
 
 # Load the CLIP model and processor
-model = CLIPModel.from_pretrained("openai/clip-vit-base-patch16")
-processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch16")
+model = CLIPModel.from_pretrained("openai/clip-vit-base-patch16", cache_dir="./models/clip-vit-base-patch16")
+processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch16", cache_dir="./models/clip-vit-base-patch16")
+
 
 search_params = {
     "metric_type": "L2", 
